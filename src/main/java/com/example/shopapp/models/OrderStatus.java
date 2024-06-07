560e0dfe235f8808e5b1f725ba8a29b6573325b3
htkiet <htkiet@vnpt.vn>
2024-06-07 16:39:03 +0700

    .

--- src/main/java/com/example/shopapp/models/OrderStatus.java
+++
@@ -1,9 +0,0 @@
-package com.example.shopapp.models;
-
-public class OrderStatus {
-    public static final String PENDING ="pending";
-    public static final String PROCESSING ="processing";
-    public static final String SHIPPED ="shopped";
-    public static final String DELIVERED ="delivered";
-    public static final String CANCELLED ="cancelled";
-}
